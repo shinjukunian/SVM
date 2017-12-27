ae1fa2d41d1f889fad0ba4d989c41be1d940bb50
Morten Bertz <morten@telethon.jp>
2017-12-27 13:51:02 +0900

    training maybe works

--- SVM.cpp
+++ SVM.cpp
@@ -45,18 +45,15 @@
 
 #include "XOPStandardHeaders.h"			// Include ANSI headers, Mac headers, IgorXOP.h, XOP.h and XOPSupport.h
 #include "SVM.h"
+#include "libSVM/svm.h"
 
 // Global Variables
 
 
 
 
-
-/*	RegisterFunction()
-	
-	Igor calls this at startup time to find the address of the
-	XFUNCs added by this XOP. See XOP manual regarding "Direct XFUNCs".
-*/
+#define Malloc(type,n) (type *)malloc((n)*sizeof(type))
+svm_problem makeProblem(waveHndl inputWave, waveHndl inputClasses);
 
 
 
@@ -138,60 +135,185 @@ typedef struct SVMTrainRuntimeParams* SVMTrainRuntimeParamsPtr;
 extern "C" int
 ExecuteSVMTrain(SVMTrainRuntimeParamsPtr p)
 {
+    struct svm_parameter params={0};
+    params.cache_size=100;
+    params.eps=0.001;
+    char outPutPath[MAX_PATH_LEN+1]="model.svm";
     int err = 0;
+    struct svm_problem problem={0};
     
     // Flag parameters.
     
-    if (p->TYPEFlagEncountered) {
+    if (p->TYPEFlagEncountered && p->svm_type<5) {
         // Parameter: p->svm_type
+        params.svm_type=(int)p->svm_type;
+    }
+    else{
+        return INCOMPATIBLE_FLAGS;
+        
     }
     
-    if (p->KFlagEncountered) {
+    if (p->KFlagEncountered && p->kernel_type<5) {
         // Parameter: p->kernel_type
+        params.kernel_type=(int)p->kernel_type;
+    }
+    else{
+       return INCOMPATIBLE_FLAGS;
     }
     
     if (p->DFlagEncountered) {
         // Parameter: p->degree
+        params.degree=(int)p->degree;
     }
     
     if (p->YFlagEncountered) {
         // Parameter: p->gamma
+        params.gamma=p->gamma;
     }
     
     if (p->CFFlagEncountered) {
         // Parameter: p->coef0
+        params.coef0=p->coef0;
     }
     
     if (p->CFlagEncountered) {
         // Parameter: p->C
+        params.C=p->C;
     }
     
     if (p->NUFlagEncountered) {
         // Parameter: p->nu
+        params.nu=p->nu;
     }
     
     if (p->SHRINKFlagEncountered) {
+        params.shrinking=1;
     }
     
     if (p->PROBFlagEncountered) {
+        params.probability=1;
     }
     
     // Main parameters.
     
     if (p->outputPathEncountered) {
         // Parameter: p->outPutPath
+        ConcatenatePaths(p->outPutPath, "model.svm", outPutPath);
     }
+    else if (XOPSaveFileDialog("Select where to save the model file", "", NULL, "", "svm", outPutPath) != 0){
+         return FILE_NOT_FOUND;
+    }
+   
     
-    if (p->inputWaveEncountered) {
+    if (p->inputWaveEncountered && p->inPutWave != NULL) {
         // Parameter: p->inPutWave (test for NULL handle before using)
+        if (p->inputClassesEncountered && p->inputClasses != NULL) {
+            // Parameter: p->inputClasses (test for NULL handle before using)
+            int numDimensionsInputWave;
+            int numDimensionsClassesWave;
+            CountInt dimensionSizesClassesWave[MAX_DIMENSIONS+1];
+            CountInt dimensionSizesInputWave[MAX_DIMENSIONS+1];
+            int resultClassesWave=MDGetWaveDimensions(p->inPutWave, &numDimensionsInputWave, dimensionSizesInputWave);
+            int resultInPutWave=MDGetWaveDimensions(p->inputClasses, &numDimensionsClassesWave, dimensionSizesClassesWave);
+            if (resultInPutWave || resultClassesWave){
+                return resultInPutWave;
+            }
+            else if(numDimensionsInputWave<1){
+                return EXPECT_MATRIX;
+            }
+            else if (dimensionSizesClassesWave[0] != dimensionSizesInputWave[0]){
+                return WAVE_LENGTH_MISMATCH;
+            }
+            else{
+                problem=makeProblem(p->inPutWave, p->inputClasses);
+                const char *parameterError=svm_check_parameter(&problem,
+                                                        &params);
+                if (parameterError == 0) {
+                    struct svm_model *model=svm_train(&problem, &params);
+                    double *target = Malloc(double,problem.l);
+                    int total_correct = 0;
+
+                    svm_cross_validation(&problem,&params,10,target);
+                    for(int i=0;i<problem.l;i++){
+                        if(target[i] == problem.y[i]){
+                            ++total_correct;
+                        }
+                    }
+                    free(target);
+                    printf("Cross Validation Accuracy = %g%%\n",100.0*total_correct/problem.l);
+#ifdef MACIGOR
+                    HFSToPosixPath(outPutPath, outPutPath, 0);
+#endif
+                    if(svm_save_model(outPutPath,model))
+                    {
+                        fprintf(stderr, "can't save model to file %s\n", outPutPath);
+                        
+                    }
+                    svm_free_and_destroy_model(&model);
+                    svm_destroy_param(&params);
+                    free(problem.y);
+                    free(problem.x);
+                }
+                else{
+                    
+                }
+                
+                
+            }
+           
+           
+            
+            
+        }
     }
     
-    if (p->inputClassesEncountered) {
-        // Parameter: p->inputClasses (test for NULL handle before using)
-    }
+    
     
     return err;
 }
+
+
+svm_problem makeProblem(waveHndl inputWave, waveHndl inputClasses){
+    svm_problem problem={0};
+    int numDimensionsInputWave;
+    int numDimensionsClassesWave;
+    struct svm_node *x_space={0};
+    
+    CountInt dimensionSizesClassesWave[MAX_DIMENSIONS+1];
+    CountInt dimensionSizesInputWave[MAX_DIMENSIONS+1];
+    MDGetWaveDimensions(inputWave, &numDimensionsInputWave, dimensionSizesInputWave);
+    MDGetWaveDimensions(inputClasses, &numDimensionsClassesWave, dimensionSizesClassesWave);
+    int elements=dimensionSizesInputWave[1];
+    problem.l=dimensionSizesClassesWave[0];
+    problem.y=Malloc(double, problem.l);
+    problem.x=Malloc(struct svm_node *,problem.l);
+    x_space = Malloc(struct svm_node,elements+1);
+    double value[2];
+    IndexInt classIndices[MAX_DIMENSIONS]={0};
+    IndexInt dataIndices[MAX_DIMENSIONS]={0};
+    for (int i=0; i<problem.l; i++) {
+        classIndices[0]=i;
+        MDGetNumericWavePointValue(inputClasses, classIndices, value);
+        problem.y[i]=value[0];
+        
+        for (int j=0; j<elements; j++) {
+            dataIndices[0]=i;
+            dataIndices[1]=j;
+            MDGetNumericWavePointValue(inputWave, dataIndices, value);
+            x_space[j].index=j+1;
+            x_space[j].value=value[0];
+            
+        }
+        x_space[elements].index=-1;
+        problem.x[i]=&x_space[i];
+    }
+    
+    
+    
+    return problem;
+}
+
+
 
 static int
 RegisterSVMTrain(void)
@@ -202,8 +324,8 @@ RegisterSVMTrain(void)
     
     // NOTE: If you change this template, you must change the SVMTrainRuntimeParams structure as well.
     cmdTemplate = "SVMTrain /TYPE=number:svm_type /K=number:kernel_type /D=number:degree /Y=number:gamma /CF=number:coef0 /C=number:C /NU=number:nu /SHRINK /PROB outputPath=name:outPutPath, inputWave=wave:inPutWave, inputClasses=wave:inputClasses";
-    runtimeNumVarList = "";
-    runtimeStrVarList = "";
+    runtimeNumVarList = "V_flag";
+    runtimeStrVarList = "S_fileName";
     return RegisterOperation(cmdTemplate, runtimeNumVarList, runtimeStrVarList, sizeof(SVMTrainRuntimeParams), (void*)ExecuteSVMTrain, 0);
 }
 
@@ -308,11 +430,17 @@ XOPMain(IORecHandle ioRecHandle)		// The use of XOPMain rather than main means t
 {	
 	XOPInit(ioRecHandle);				// Do standard XOP initialization.
 	SetXOPEntry(XOPEntry);				// Set entry point for future calls.
+    
+    int err;
 	
 	if (igorVersion < 620) {			// Requires Igor Pro 6.20 or later.
 		SetXOPResult(OLD_IGOR);			// OLD_IGOR is defined in SVM.h and there are corresponding error strings in SVM.r and SVMWinCustom.rc.
 		return EXIT_FAILURE;
 	}
+    if (err = RegisterSVMTrain()) {
+        SetXOPResult(err);
+        return EXIT_FAILURE;
+    }
 
 	SetXOPResult(0L);
 	return EXIT_SUCCESS;
